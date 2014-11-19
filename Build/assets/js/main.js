0e0db0b4856d066f7e92feb8ae9d88e9ee2878aa
Owain Lleweellyn <owain.llew@gmail.com>
2014-11-18 19:30:26 -0500

    added functionality for hiding and showing header/footer on scroll

--- Build/assets/js/main.js
+++ Build/assets/js/main.js
@@ -30,4 +30,96 @@
 	   }
 	});
 
+    app.directive('header', function() {
+        return {
+            restrict: 'ECA',
+            controller: "appCtrl",
+            link: function(scope, el, attrs, ctrl) {
+                var didScroll;
+                var lastScrollTop = 0;
+                var delta = 5;
+                var navbarHeight = $('header').outerHeight();
+
+                $(window).scroll(function(event){
+                    didScroll = true;
+                });
+
+                setInterval(function() {
+                    if (didScroll) {
+                        hasScrolled();
+                        didScroll = false;
+                    }
+                }, 250);
+
+                function hasScrolled() {
+                    var st = $(window).scrollTop();
+                    
+                    // Make sure they scroll more than delta
+                    if(Math.abs(lastScrollTop - st) <= delta)
+                        return;
+                    
+                    // If they scrolled down and are past the navbar, add class .nav-up.
+                    // This is necessary so you never see what is "behind" the navbar.
+                    if (st > lastScrollTop && st > navbarHeight){
+                        // Scroll Down
+                        $('header').removeClass('show').addClass('hide');
+                    } else {
+                        // Scroll Up
+                        if(st + $(window).height() < $(document).height()) {
+                            $('header').removeClass('hide').addClass('show');
+                        }
+                    }
+                    
+                    lastScrollTop = st;
+                }
+            }
+        }
+    });
+
+    app.directive('footer', function() {
+            return {
+                restrict: 'ECA',
+                controller: "appCtrl",
+                link: function(scope, el, attrs, ctrl) {
+                    var didScroll;
+                    var lastScrollTop = 0;
+                    var delta = 5;
+                    var navbarHeight = $('footer').outerHeight();
+
+                    $(window).scroll(function(event){
+                        didScroll = true;
+                    });
+
+                    setInterval(function() {
+                        if (didScroll) {
+                            hasScrolled();
+                            didScroll = false;
+                        }
+                    }, 250);
+
+                    function hasScrolled() {
+                        var st = $(window).scrollTop();
+                        
+                        // Make sure they scroll more than delta
+                        if(Math.abs(lastScrollTop - st) <= delta)
+                            return;
+                        
+                        // If they scrolled down and are past the navbar, add class .nav-up.
+                        // This is necessary so you never see what is "behind" the navbar.
+                        if (st > lastScrollTop && st > navbarHeight){
+                            // Scroll Down
+                            $('footer').removeClass('show').addClass('hide');
+                        } else {
+                            // Scroll Up
+                            if(st + $(window).height() < $(document).height()) {
+                                $('footer').removeClass('hide').addClass('show');
+                            }
+                        }
+                        
+                        lastScrollTop = st;
+                    }
+                }
+            }
+        });
+
 }());
